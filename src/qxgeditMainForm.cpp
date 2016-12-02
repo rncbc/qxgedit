409b698d1328dfd141cfb827e9da1cf2054d5cf3
rncbc <rncbc@rncbc.org>
2016-12-02 11:05:59 +0000

    - Fixed some line-breaks on the about box display.

--- src/qxgeditMainForm.cpp
+++ src/qxgeditMainForm.cpp
@@ -1843,7 +1843,6 @@ void qxgeditMainForm::helpAbout (void)
 	sText += tr("Version") + ": <b>" CONFIG_BUILD_VERSION "</b><br />\n";
 //	sText += "<small>" + tr("Build") + ": " CONFIG_BUILD_DATE "</small><br />\n";
 #ifdef CONFIG_DEBUG
-	sText += "<br />\n";
 	sText += "<small><font color=\"red\">";
 	sText += tr("Debugging option enabled.");
 	sText += "</font></small><br />\n";
