6fb578e8fd618454b7a3486ee5b032b066e0b8ae
rncbc <rncbc@rncbc.org>
2025-09-13 18:57:18 +0100

    - Get rid of CONFIG_WAYLAND build config option.

--- src/config.h.cmake
+++ src/config.h.cmake
@@ -53,8 +53,5 @@
 /* Define if debugger stack-trace is enabled. */
 #cmakedefine CONFIG_STACKTRACE @CONFIG_STACKTRACE@
 
-/* Define if Wayland is supported */
-#cmakedefine CONFIG_WAYLAND @CONFIG_WAYLAND@
-
 
 #endif /* CONFIG_H */
