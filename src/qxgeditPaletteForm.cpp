d4e659e33dcd3f9c4602f5d9002af0e887f239c2
rncbc <rncbc@rncbc.org>
2026-07-30 22:27:38 +0100

    - Fixed mem-leak on Custom color theme (palette) editor.

--- src/qxgeditPaletteForm.cpp
+++ src/qxgeditPaletteForm.cpp
@@ -1,7 +1,7 @@
 // qxgeditPaletteForm.cpp
 //
 /****************************************************************************
-   Copyright (C) 2005-2024, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2005-2026, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -161,6 +161,8 @@ qxgeditPaletteForm::qxgeditPaletteForm ( QWidget *parent, const QPalette& pal )
 qxgeditPaletteForm::~qxgeditPaletteForm (void)
 {
 	setSettings(nullptr);
+
+	delete p_ui;
 }
 
 
